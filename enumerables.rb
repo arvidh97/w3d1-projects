8422c94f40de9527e42ee00b0cbc34d4900ef86d
Tyvan Cheng <tyvancheng@gmail.com>
2023-03-20 16:37:29 -0400

    Added reject, any, all

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,4 @@
+# require "byebug"
 class Array 
 
     def my_each(&blk)
@@ -5,15 +6,36 @@ class Array
             blk[self[i]]
         end
         self
+        # self.map {|el| blk[el]}
     end
 
     def my_select(&blk)
+        # debugger
         new_arr = []
         self.my_each(&blk).each_with_index do |ele, i| 
-            new_arr << self[i] if ele == true
+            new_arr << self[i] if blk[ele] == true
         end
         new_arr
     end
-end
+    
+    def my_reject(&blk)
+        arr = []
+        self.each_with_index do |ele,i|
+            arr << self[i] if blk[ele]==false
+        end
+        arr
+    end
 
+    def my_any?(&blk)
+        count = 0
+        self.each {|el| count+=1 if blk[el] == true}
+        count >= 1
+    end
+    
+    def my_all?(&blk)
+        count = 0
+        self.each {|el| count+=1 if blk[el] == true}
+        count == self.length
+    end
+end
 
